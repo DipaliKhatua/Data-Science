183155aa71bbb75f581f0ed306ee855349df22c2
DipaliKhatua <khatuadipsha@gmail.com>
2025-04-05 10:11:48 +0530

    Solved All Errors

--- End to End Data Science Project/3. Complete DS Project/app.py
+++ End to End Data Science Project/3. Complete DS Project/app.py
@@ -1,30 +1,31 @@
 from src.DSProject.logger import logging
 from src.DSProject.exception import CustomException
 from src.DSProject.components.data_ingestion import DataIngestion
-from src.DSProject.components.data_ingestion import DataIngestionConfig
-from src.DSProject.components.data_transformation import DataTransformationConfig,DataTransformation
-from src.DSProject.components.model_trainer import ModelTrainerConfig,ModelTrainer
+from src.DSProject.components.data_transformation import DataTransformation
+from src.DSProject.components.model_trainer import ModelTrainer
 
 import sys
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
     logging.info("The execution has started")
 
     try:
-        #data_ingestion_config=DataIngestionConfig()
-        data_ingestion=DataIngestion()
-        train_data_path,test_data_path=data_ingestion.initiate_data_ingestion()
+        # Data Ingestion
+        data_ingestion = DataIngestion()
+        train_data_path, test_data_path = data_ingestion.initiate_data_ingestion()
 
-        #data_transformation_config=DataTransformationConfig()
-        data_transformation=DataTransformation()
-        train_arr,test_arr,_=data_transformation.initiate_data_transormation(train_data_path,test_data_path)
+        logging.info(f"Train data path: {train_data_path}")
+        logging.info(f"Test data path: {test_data_path}")
 
-        ## Model Training
+        # Data Transformation
+        data_transformation = DataTransformation()
+        train_arr, test_arr, _ = data_transformation.initiate_data_transormation(train_data_path, test_data_path)
 
-        model_trainer=ModelTrainer()
-        print(model_trainer.initiate_model_trainer(train_arr,test_arr))
-        
+        # Model Training
+        model_trainer = ModelTrainer()
+        print(model_trainer.initiate_model_trainer(train_arr, test_arr))
+
     except Exception as e:
         logging.info("Custom Exception")
-        raise CustomException(e,sys)
+        raise CustomException(str(e), sys)
